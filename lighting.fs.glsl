bca03c7b2fe2a03e5150509c84c37efb03ed4aae
Rahul Kumar <rahul@thoughtnirvana.com>
2019-01-07 23:16:31 +0530

    Shaders for lighting

---
+++ lighting.fs.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+
+void main()
+{
+	FragColor = mix(
+		texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2
+	) * vec4(lightColor * objectColor, 1.0);
+}

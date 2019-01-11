366ccd762773625aa4e4c8ba2f46c994e56cac64
Rahul Kumar <rahul@thoughtnirvana.com>
2019-01-11 13:30:26 +0530

    Changed light position

--- src/maps.cpp
+++ src/maps.cpp
@@ -32,7 +32,7 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 // lighting
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(0.8f, 0.8f, 0.8f);
 
 int main()
 {
@@ -204,6 +204,7 @@ int main()
 
         // world transformation
         glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
         lightingShader.setMat4("model", model);
 
         // bind diffuse map
@@ -221,7 +222,7 @@ int main()
         lampShader.setMat4("view", view);
         model = glm::mat4(1.0f);
         model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+        model = glm::scale(model, glm::vec3(0.1f)); // a smaller cube
         lampShader.setMat4("model", model);
 
         glBindVertexArray(lightVAO);

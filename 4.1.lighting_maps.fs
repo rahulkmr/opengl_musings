15577bf4f744292d7e43deead38b1b5b73717be0
Rahul Kumar <rahul@thoughtnirvana.com>
2019-01-11 13:22:50 +0530

    Lighting map shaders

--- 4.1.lighting_maps.fs
+++ 4.1.lighting_maps.fs
@@ -1,3 +1,5 @@
+
+
 #version 330 core
 out vec4 FragColor;
 
@@ -17,7 +19,7 @@ struct Light {
 
 in vec3 FragPos;  
 in vec3 Normal;  
-in vec2 TexCoord;
+in vec2 TexCoords;
   
 uniform vec3 viewPos;
 uniform Material material;
@@ -26,13 +28,13 @@ uniform Light light;
 void main()
 {
     // ambient
-    vec3 ambient = light.ambient * texture(material.diffuse, TexCoord).rgb;
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
   	
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoord).rgb;  
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
